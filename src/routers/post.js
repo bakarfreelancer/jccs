24c403d253f2fa882ae11956caea6d6c4fce52fe
Abu Bakar <abubakarccs761@gmail.com>
2022-07-16 10:21:20 +0500

    User and Post new routes added (update, delete, read)

--- src/routers/post.js
+++ src/routers/post.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 
 router.post("/post", auth, async (req, res) => {
-  const post = new Post(req.body);
+  const post = new Post({ ...req.body, author: req.user._id });
   try {
     await post.save();
     res.status(201).send(post);
@@ -13,4 +13,88 @@ router.post("/post", auth, async (req, res) => {
   }
 });
 
+// Get all posts
+router.get("/post", auth, async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).send(posts);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// Get Post by id
+router.get("/post/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).send();
+    }
+    res.status(200).send(post);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// Get posts by user id
+router.get("/post/user/:userId", auth, async (req, res) => {
+  try {
+    const posts = await Post.find({ author: req.params.userId });
+    res.status(200).send(posts);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// Get public posts
+router.get("/publicPost/", async (req, res) => {
+  try {
+    const posts = await Post.find({
+      visibility: "Public",
+      status: "Published",
+    });
+    res.status(200).send(posts);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// Get Published Posts
+router.get("/publishedPost/", async (req, res) => {
+  try {
+    const posts = await Post.find({ status: "Published" });
+    res.status(200).send(posts);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// Update Post
+router.patch("/post/:id", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  try {
+    const post = await Post.findById(req.params.id);
+    updates.forEach((update) => (post[update] = req.body[update]));
+    await post.save();
+    if (!post) {
+      return res.status(404).send();
+    }
+    res.send(post);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// Delete post by id
+router.delete("/post/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+    if (!post) {
+      return res.status(404).send();
+    }
+    res.send(post);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 module.exports = router;
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const User = require("../models/user");
 const router = new express.Router();
 const auth = require("../middleware/auth");
 const res = require("express/lib/response");
+const { TokenExpiredError } = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   const user = new User(req.body);
@@ -27,6 +28,43 @@ router.post("/login", async (req, res) => {
     }
     const token = await user.generateAuthToken();
     res.status(200).send({ user, token });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.post("/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.post("/logoutall", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+// Update user details
+router.patch("/user/:id", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  try {
+    const user = await User.findById(req.params.id);
+    updates.forEach((update) => {
+      user[update] = req.body[update];
+    }),
+      await user.save();
+    res.send(user);
   } catch (e) {
     res.status(400).send(e);
   }
